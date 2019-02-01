0d0812c390e9531cb604089de7e4e93adbaca819
Noel Nevarez <noel3740@gmail.com>
2019-01-31 22:40:53 -0600

    Prevent devour button from running if customer name not entered.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,22 +25,43 @@ $(document).ready(() => {
         this.classList.add('was-validated');
     });
 
-    $(".devourButton").on("click", function (event) {
-        var id = $(this).data("burger-id");
+    $(".devourForm").on("submit", function (event) {
+        //Prevent the screen from being refreshed on submit
+        event.preventDefault();
 
-        //Create a new burger object that only contains the devoured set to true
-        var newBurgerState = {
-            devoured: 1
-        };
+        //Check for errors in required fields and prevent post to API if there are errors. 
+        if (this.checkValidity()) {
+            var id = $(this.customerName).data("burger-id");
 
-        //Send the PUT request.
-        $.ajax(`/api/burgers/${id}`, {
-            type: "PUT",
-            data: newBurgerState
-        }).then(function () {
+            //Create the customer in the database
+            var newCustomer = {
+                customer_name: this.customerName.value.trim()
+            }
 
-            //Hide burger item with animation 
-            $(event.target).parent().hide({ duration: 500, complete: () => location.reload() });
-        });
+            //Send the POST request to create the customer.
+            $.ajax(`/api/customers`, {
+                type: "POST",
+                data: newCustomer
+            }).then(function (customerId) {
+
+                //Create a new burger object that only contains the devoured set to true and the customer id set to whatever customer the user entered
+                var newBurgerState = {
+                    devoured: 1,
+                    CustomerId: customerId
+                };
+
+                //Send the PUT request.
+                $.ajax(`/api/burgers/${id}`, {
+                    type: "PUT",
+                    data: newBurgerState
+                }).then(function () {
+
+                    //Hide burger item with animation 
+                    $(event.target).parent().hide({ duration: 500, complete: () => location.reload() });
+                });
+            });
+        }
+
+        this.classList.add('was-validated');
     });
 });
