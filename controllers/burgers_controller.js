98c77051f51bac906309f1683a5f4a6320974279
Noel Nevarez <noel3740@gmail.com>
2019-01-31 22:53:22 -0600

    Adding comments to controllers.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,9 @@ const db = require("../models");
 module.exports = (app) => {
     app
         .get("/api/burgers", (req, res) => {
+            //Find all burgers in the db and left join 
+            //the customers associated with the burger
+            //Return the results in JSON
             db.Burger.findAll({
                 include: [db.Customer]
             })
@@ -13,6 +16,9 @@ module.exports = (app) => {
                 });
         })
         .post("/api/burgers", (req, res) => {
+            //Create the burger in the db then 
+            //return the new burger id in JSON format.
+            //If there is an error then send a 500 error
             db.Burger
                 .create({
                     burger_name: req.body.burger_name,
@@ -27,19 +33,24 @@ module.exports = (app) => {
                 });
         })
         .put("/api/burgers/:id", (req, res) => {
+            //Build burger object that represents the values we will update in the db
             const updatedBurger = {
                 devoured: req.body.devoured,
                 CustomerId: req.body.CustomerId
             };
 
+            //Build the query object used in the sequelize update
             const query = {
                 where: {
                     id: req.params.id
                 }
             };
 
+            //Update the burger in the db using the updated burger and query objects
             db.Burger.update(updatedBurger, query)
                 .then(result => {
+                    //If index 0 of the result object is 0 or less (no rows updated) 
+                    //then return a 404 status else return a 200/success status
                     if (result[0] <= 0) {
                         res.status(404).send("No rows found to update!").end()
                     } else {
@@ -47,6 +58,7 @@ module.exports = (app) => {
                     }
                 })
                 .catch(err => {
+                    //Log the error in the node console and return a 500 status
                     console.log(err);
                     res.status(500).send("Failed updating burger in database");
                 });
--- controllers/customers_controller.js
+++ controllers/customers_controller.js
@@ -5,6 +5,8 @@ const db = require("../models");
 module.exports = (app) => {
     app
         .get("/api/customers", (req, res) => {
+            //Find all customers in the db and left join the burgers associated with the customer
+            //Return results in JSON
             db.Customer.findAll({
                 include: [db.Burger]
             })
@@ -30,23 +32,31 @@ module.exports = (app) => {
                     res.json(result[0].id);
                 })
                 .catch(err => {
+                    //If there is an error then log the error in the node console
+                    //and return a 500 status
                     console.log(err);
                     res.status(500).send("Failed saving customer to database");
                 });
         })
         .put("/api/customers/:id", (req, res) => {
+
+            //Create an object that represents the customer object we will update in the db
             const updatedCustomer = {
                 customer_name: req.body.customer_name
             };
 
+            //Build the query object used in the sequelize update
             const query = {
                 where: {
                     id: req.params.id
                 }
             };
 
+            //Update the customer in the db using the updated customer and query objects
             db.Customer.update(updatedCustomer, query)
                 .then(result => {
+                    //If index 0 of the result object is 0 or less (no rows updated) 
+                    //then return a 404 status else return a 200/success status
                     if (result[0] <= 0) {
                         res.status(404).send("No rows found to update!").end()
                     } else {
@@ -54,6 +64,7 @@ module.exports = (app) => {
                     }
                 })
                 .catch(err => {
+                    //Log the error in the node console and return a 500 status
                     console.log(err);
                     res.status(500).send("Failed updating customer in database");
                 });
