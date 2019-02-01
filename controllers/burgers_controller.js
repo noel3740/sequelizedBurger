55683fe048773bfcc8a7c235b5af80832bb2ad65
Noel Nevarez <noel3740@gmail.com>
2019-01-31 22:37:46 -0600

    Modifying burger controller so it exports a function.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,61 +1,54 @@
 //Add dependencies
-const express = require("express");
 const db = require("../models");
 
-//Initialize the express router
-const router = express.Router();
+//Create all the routes and setup logic withiin each route
+module.exports = (app) => {
+    app
+        .get("/api/burgers", (req, res) => {
+            db.Burger.findAll({
+                include: [db.Customer]
+            })
+                .then(results => {
+                    res.json(results);
+                });
+        })
+        .post("/api/burgers", (req, res) => {
+            db.Burger
+                .create({
+                    burger_name: req.body.burger_name,
+                    devoured: req.body.devoured
+                })
+                .then(result => {
+                    res.json(result.dataValues.id);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).send("Failed saving burger to database");
+                });
+        })
+        .put("/api/burgers/:id", (req, res) => {
+            const updatedBurger = {
+                devoured: req.body.devoured,
+                CustomerId: req.body.CustomerId
+            };
 
-//Create all the routs and setup logic withiin each route
-router
-    .get("/", (req, res) => {
-        db.Burger.findAll()
-            .then(results => {
-                const burgerObject = {
-                    burgers: results
-                };
-                res.render("index", burgerObject);
-            });
-    })
-    .get("/api/burgers", (req, res) => {
-        db.Burger.findAll()
-            .then(results => {
-                res.json(results);
-            });
-    })
-    .post("/api/burgers", (req, res) => {
-        db.Burger
-            .create({
-                burger_name: req.body.burger_name,
-                devoured: req.body.devoured
-            })
-            .then(result => {
-                res.json(result.dataValues.id);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).send("Failed saving burger to database");
-            });
-    })
-    .put("/api/burgers/:id", (req, res) => {
-        db.Burger
-            .update({
-                devoured: req.body.devoured
-            }, {
-                    where: {
-                        id: req.params.id
+            const query = {
+                where: {
+                    id: req.params.id
+                }
+            };
+
+            db.Burger.update(updatedBurger, query)
+                .then(result => {
+                    if (result[0] <= 0) {
+                        res.status(404).send("No rows found to update!").end()
+                    } else {
+                        res.status(200).end();
                     }
                 })
-            .then(result => {
-                if (result[0] <= 0) {
-                    res.status(404).send("No rows found to update!").end()
-                } else {
-                    res.status(200).end();
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).send("Failed updating burger in database");
-            });
-    });
-
-module.exports = router;
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).send("Failed updating burger in database");
+                });
+        });
+}
